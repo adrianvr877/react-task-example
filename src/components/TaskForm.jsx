ae78a07fac61719ffbac1727c15bfd46f1fb4f67
Adrián Velázquez Rocha <avelazquez@cpalianza.com.mx>
2022-09-06 12:11:51 -0500

    first commit-deploy

---
+++ src/components/TaskForm.jsx
@@ -0,0 +1,42 @@
+import { useState, useContext } from "react";
+import { TaskContext } from "../context/TaskContext";
+
+function TaskForm() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const { createTask } = useContext(TaskContext);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    createTask({
+      title,
+      description,
+    });
+    setTitle("");
+    setDescription("");
+  };
+
+  return (
+    <div className="max-w-md mx-auto">
+      <form onSubmit={handleSubmit} className="bg-slate-800 p-10 mb-4">
+        <h1 className="text-2xl font-bold text-white mb-3">Captura una nueva tarea</h1>
+        <input
+          placeholder="ingresa tu tarea"
+          onChange={(e) => setTitle(e.target.value)}
+          className="bg-slate-300 p-3 w-full mb-2"
+          value={title}
+          autoFocus
+        />
+        <textarea
+          placeholder="ingresa la descrpcion de la tarea"
+          className="bg-slate-300 p-3 w-full mb-2"
+          onChange={(e) => setDescription(e.target.value)}
+          value={description}
+        ></textarea>
+        <button className="bg-green-700 text-white px-3 py-1">Guardar</button>
+      </form>
+    </div>
+  );
+}
+
+export default TaskForm;
---
+++ src/components/TaskList.jsx
@@ -0,0 +1,21 @@
+import TaskCard from "./TaskCard";
+import { useContext } from "react";
+import { TaskContext } from "../context/TaskContext";
+
+function Tasklist() {
+  const { tasks } = useContext(TaskContext);
+
+  if (tasks.length === 0) {
+    return <h1 className="text-white text-4xl font-bold text-center">Aun no hay tareas</h1>;
+  }
+
+  return (
+    <div className="grid grid-cols-4 gap-1">
+      {tasks.map((task) => (
+        <TaskCard key={task.id} task={task} />
+      ))}
+    </div>
+  );
+}
+
+export default Tasklist;
